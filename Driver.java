8e5b64a319e28bdda4445fe04a523b4ea7d9daa9
Hodge-zf <xx.skcs@gmail.com>
2017-04-05 11:19:51 -0500

    Cleans print statements

--- Driver.java
+++ Driver.java
@@ -1,14 +1,14 @@
 public class Driver{
   public static void main(String[] args){
     Example example = new Example();
-    Example injectedExample = new Example(() -> System.out.println("Injected Optional"));
+    Example injectedExample = new Example(() -> System.out.println("Optional Injected"));
 
-    InterfaceInjectable interfaceInjectable = () -> System.out.println("mystery method injected");
+    InterfaceInjectable interfaceInjectable = () -> System.out.println("Mystery Method Injected");
 
     example.doOptional();
-    example.doMysterious(() -> System.out.println("interfaceInjectable"));
+    example.doMysterious(() -> System.out.println("Interface Injectable"));
 
     injectedExample.doOptional();
-    injectedExample.doMysterious(() -> System.out.println("interfaceInjectable"));
+    injectedExample.doMysterious(() -> System.out.println("Interface Injectable"));
   }
 }
